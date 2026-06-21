b852a8866079119b2f5243b8939b2174505543c9
Alamin4D <alaminahmed79000@gmail.com>
2026-06-21 20:17:59 +0600

    first commit

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import app from "./app";
+const port = process.env.PORT || 3000
+
+const main = async () => {
+    try {
+        app.listen(port, () => {
+            console.log(`Server running on port ${port}`)
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+main();
