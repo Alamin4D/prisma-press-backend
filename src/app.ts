88f6a0ef86e900362ad7250379ef3de73474e9ac
Alamin4D <alaminahmed79000@gmail.com>
2026-06-22 14:44:30 +0600

    chore: initialize Prisma schema and database configuration

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,24 @@
-import express, { Application } from "express";
+import cookieParser from "cookie-parser";
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+import config from "./config";
+import { userRoutes } from "./modules/users/user.route";
 
 const app: Application = express();
+
+app.use(cors({
+    origin: config.app_url,
+    credentials: true,
+}))
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.get("/", async (req: Request, res: Response) => {
+    res.send("Hello World")
+})
+
+app.use("/api/users", userRoutes);
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,18 @@
 import app from "./app";
-const port = process.env.PORT || 3000
+import config from "./config";
+import { prisma } from "./lib/prisma";
 
 const main = async () => {
     try {
-        app.listen(port, () => {
-            console.log(`Server running on port ${port}`)
+        await prisma.$connect()
+        console.log("Connected to the database successfully")
+        app.listen(config.port, () => {
+            console.log(`Server running on port ${config.port}`)
         })
     } catch (error) {
         console.log(error)
+        await prisma.$disconnect();
+        process.exit(1);
     }
 }
 
