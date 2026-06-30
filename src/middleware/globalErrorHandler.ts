1a56a374b6d5d9548d972c7800d6733aba567cdf
Alamin4D <alaminahmed79000@gmail.com>
2026-06-30 12:58:22 +0600

    feat: handle Prisma record not found exceptions

--- src/middleware/globalErrorHandler.ts
+++ src/middleware/globalErrorHandler.ts
@@ -8,7 +8,7 @@ export const globalErrorHandler = (err: any, req: Request, res: Response, next: 
     let statusCode;
     let errorMessage = err.message || "Internal Server Error";
     let errorName = err.name || "Internal Server Error";
-    // let errorDetails = err.stack
+    let errorDetails = err.stack
 
     if(err instanceof Prisma.PrismaClientValidationError){
         statusCode = httpStatus.BAD_REQUEST;
